858cd9a2a9a6368f7c375d74be51a55d452d6aa0
Sasha Hart <sashanicolehart@gmail.com>
2018-08-25 02:41:08 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 // Add your functions and code here
-
+var kitens = ['Milo', 'Otis', 'Garfeild'];
 
 function destructivelyAppendKitten(name) {
-  kittens
+  kittens.push(name);
+  return kittens;
 }
