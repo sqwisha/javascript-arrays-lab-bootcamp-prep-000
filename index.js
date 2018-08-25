b45aff028e02ec20669c78a51ef419ada1281e77
Sasha Hart <sashanicolehart@gmail.com>
2018-08-25 02:50:11 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -27,6 +27,6 @@ function destructivelyRemoveFirstKitten() {
 }
 
 function prependKitten(name) {
-  var prependedKitten = name.concat(kittens);
+  var prependedKitten = kittens.splice(0, 0, name);
   return prependedKitten;
 }
